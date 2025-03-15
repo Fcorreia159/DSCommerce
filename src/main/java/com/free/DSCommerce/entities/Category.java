ef1ce9834f3ccfb14b18c905338a9d81524a4078
Filipe <correia481@gmail.com>
2025-03-15 20:32:50 -0300

    Adição das calsses Category, Payment e Product.

---
+++ src/main/java/com/free/DSCommerce/entities/Category.java
@@ -0,0 +1,26 @@
+package com.free.DSCommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+}
